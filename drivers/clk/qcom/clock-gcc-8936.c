8075f2a51f0a73706d7261e918d229189338360e
AndyPhoenix9879 <Lai9879M@gmail.com>
2017-05-23 20:52:15 +0800

    GPU: Replace 250MHz for 275MHz

--- drivers/clk/qcom/clock-gcc-8936.c
+++ drivers/clk/qcom/clock-gcc-8936.c
@@ -834,8 +834,9 @@ static struct clk_freq_tbl ftbl_gcc_oxili_gfx3d_clk[] = {
 	F( 178000000,      gpll3_out_main,   5,	  0,	0),
 	F( 192000000,      gpll0_out_main,   4,	  0,	0),
 	F( 200000000,      gpll0_out_main,   4,	  0,	0),
-	F( 250000000,      gpll3_out_main,   5,	  0,	0),
+	F( 250000000,      gpll0_out_main,   5,	  0,	0),
 	F( 266670000,      gpll0_out_main,   3,	  0,	0),
+	F( 275000000,      gpll3_out_main,   3,   0,    0),
 	F( 310000000,	gpll2_gfx3d,	3,	  0,	0),
 	F( 415000000,	   gpll3_out_main,   2,	  0,	0),
 	F( 435000000,      gpll3_out_main,   2,	  0,	0),
@@ -853,8 +854,8 @@ static struct rcg_clk gfx3d_clk_src = {
 	.c = {
 		.dbg_name = "gfx3d_clk_src",
 		.ops = &clk_ops_rcg,
-		VDD_DIG_FMAX_MAP4(LOW, 220000000, NOMINAL, 400000000, NOMINAL_PLUS, 465000000, HIGH,
-			550000000),
+		VDD_DIG_FMAX_MAP4(LOW, 178000000, NOMINAL, 435000000, NOMINAL_PLUS, 650000000, HIGH,
+			700000000),
 		CLK_INIT(gfx3d_clk_src.c),
 	},
 };
