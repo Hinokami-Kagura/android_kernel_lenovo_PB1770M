0199137f8bc38021408c531c4014004eda47c556
Ricky Cheung <rcheung844@gmail.com>
2020-07-03 14:52:51 +0800

    drivers: media: camera_v2: lenovo stuff

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#1 SMP PREEMPT Fri Jul 3 14:45:45 HKT 2020"
+#define LINUX_COMPILE_BY "ricky"
+#define LINUX_COMPILE_HOST "ricky-ThinkCentre-M92p"
+#define LINUX_COMPILER "gcc version 4.9.4 20150720 (prerelease) (UBERTC-4.9.4) "
---
+++ out/include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.10.108-lineageos-gd844fadd856-dirty"
